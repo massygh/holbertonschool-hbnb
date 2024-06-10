2836b7a3878f5697e597be3cf3208a81c5e54f55
massygh <massyghen1@gmail.com>
2024-06-10 17:36:40 +0200

    correction

--- run.py
+++ run.py
@@ -15,5 +15,5 @@ app = Flask(__name__)
 app.register_blueprint(api_app)
 
 # Run the Flask application
-if __name__ == "_main_":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
