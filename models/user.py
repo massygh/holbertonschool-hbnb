9751ab758b6e9e45fef0fac60df1848c29405d5a
Ines-Oubabas <inesoubabas56@gmail.com>
2024-06-11 15:30:54 +0200

    fichier user modifié

--- models/user.py
+++ models/user.py
@@ -11,12 +11,45 @@ class User:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+    def to_dict(self):
+        """
+        Convert the user object to a dictionary.
+        """
+        return {
+            'id': str(self.id),
+            'email': self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat(),
+        }
+
+    def update(self, email=None, first_name=None, last_name=None, password=None):
+        """
+        Update the user attributes.
+        """
+        if email:
+            self.email = email
+        if first_name:
+            self.first_name = first_name
+        if last_name:
+            self.last_name = last_name
+        if password:
+            self.password = password
+        self.updated_at = datetime.now()
+
+    @staticmethod
+    def is_email_unique(email, users):
+        """
+        Check if the email is unique among a list of users.
+        """
+        for user in users:
+            if user.email == email:
+                return False
+        return True
+
     def createUser(self):
         pass  # implementation of user creation
 
     def deleteUser(self):
         pass  # implementation of user deletion
-
-    @staticmethod
-    def is_email_unique(email):
-        pass  # Logic to check if the email is unique in the system
